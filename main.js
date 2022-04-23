63ef0a0611b33f736954ed668d3690716836593d
ArthurGuiS <arthur12304@hotmail.com>
2022-04-23 15:17:46 -0300

    adicionado funcao teclaPom

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.querySelector('.tecla_pom')
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play()
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom //nao utiliza o () depois do tocaSomPom para nao ser executado assim que a pagina carregar
