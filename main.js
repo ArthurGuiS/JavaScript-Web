72a989b3e46c758a957cf2633e4141c3f873a501
ArthurGuiS <arthur12304@hotmail.com>
2022-04-25 11:26:07 -0300

    Projeto Finalizado

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom //nao utiliza o () depois do tocaSomPom para nao ser executado assim que a pagina carregar
+// document.querySelector('.tecla_pom').onclick = tocaSomPom //nao utiliza o () depois do tocaSomPom para nao ser executado assim que a pagina carregar
+
+const listaDeTeclas = document.querySelectorAll(".tecla")
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i]
+
+    const instrumento = tecla.classList[1]
+
+    const idAu = `#som_${instrumento}`
+    
+    tecla.onclick = function (){
+        tocaSom(idAu)
+    }
+    
+}
